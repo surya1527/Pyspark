01fced2666d21d2f7c011d9afe8f73ad8b8cfae1
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-18 19:33:07 +0530

    Update change_all_column_types.py

--- change_all_column_types.py
+++ change_all_column_types.py
@@ -3,4 +3,4 @@ def change_column_type_all(source_df, cast_type):
     change_column_type_df = source_df.select(
         [source_df.cast(cast_type).alias(c) for c in source_df.columns]
     )
-    return change_column_type_df
+    return change_column_type_df 

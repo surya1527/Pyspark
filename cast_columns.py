8dd56f828af4cb530b35f8db66c71615851e7949
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-18 19:32:52 +0530

    Update cast_columns.py

--- cast_columns.py
+++ cast_columns.py
@@ -3,4 +3,4 @@ def cast_column(source_df, column_name, cast_type):
     cast_df = source_df.withColumn(
         column_name, source_df[column_name].cast(cast_type)
     )
-    return cast_df
+    return cast_df 

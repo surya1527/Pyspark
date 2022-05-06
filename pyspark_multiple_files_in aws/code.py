17a87017404ad938bb224fc813567e067e8b2376
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-05-06 20:39:16 +0530

    Update code.py

--- pyspark_multiple_files_in aws/code.py
+++ pyspark_multiple_files_in aws/code.py
@@ -1,1 +1,7 @@
- 
+from pyspark.sql.functions import input_file_name
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('sparkSesss').getOrCreate()
+
+TABLE_NAMES = ["file_data_object.csv", "file_data_part.csv", "file_data_events.csv"]
+source_path = "s3://trial-bucket-11/trial"

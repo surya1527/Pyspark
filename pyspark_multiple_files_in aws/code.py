635d8cfe5a10a96bf1a67bb7e0fadda69d13c4d8
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-05-06 20:46:40 +0530

    Update code.py

--- pyspark_multiple_files_in aws/code.py
+++ pyspark_multiple_files_in aws/code.py
@@ -7,3 +7,18 @@ TABLE_NAMES = ["file_data_object.csv", "file_data_part.csv", "file_data_events.c
 source_path = "s3://trial-bucket-11/trial"
 dest_path = "s3://trial-output/parquet_output"
 input_paths = [f"{source_path}/{t}" for t in TABLE_NAMES]
+
+
+def writecom(input_paths):
+    CSV_READ_OPTIONS = {
+                "header": True
+                }
+    for i in input_paths:
+        all_df = spark.read.csv(i,**CSV_READ_OPTIONS).withColumn("file_name", input_file_name())
+        xyz = i.split("/")[-1].split(".")[0]
+        print("xx",xyz)
+        all_df.where(all_df["file_name"].contains(xyz)).write.mode("append").parquet(f"{dest_path}/{xyz}")
+    return all_df
+
+k = writecom(input_paths)
+print("hello",k)
